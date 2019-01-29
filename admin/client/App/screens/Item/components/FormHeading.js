aa80217d451dc7b62597c5742037e386e1da8c4c
Idioteam <idiotest@nsi.it>
2019-01-29 12:25:36 +0100

    UI Heading collassabile

--- admin/client/App/screens/Item/components/FormHeading.js
+++ admin/client/App/screens/Item/components/FormHeading.js
@@ -6,10 +6,21 @@ module.exports = React.createClass({
 	propTypes: {
 		options: React.PropTypes.object,
 	},
+	getInitialState () {
+		return {
+			id: `Header-${++counter}`,
+		}
+	},
+	hide () {
+		jQuery('#' + this.state.id).toggleClass('hide-children').nextUntil('h3').toggleClass('hide-child');
+	},
 	render () {
 		if (!evalDependsOn(this.props.options.dependsOn, this.props.options.values)) {
 			return null;
 		}
-		return <h3 className="form-heading">{this.props.content}</h3>;
+		return (<h3 id={this.state.id} className="form-heading" onClick={this.hide}>
+			<i className='octicon octicon-chevron-down'></i>
+			{this.props.content}
+		</h3>);
 	},
 });
